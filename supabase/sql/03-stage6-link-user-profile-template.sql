5b562dda280c4265b7dac30f3deb086176d17835
Soukyy522 <rudigamer126@gmail.com>
2026-08-26 15:17:40 +0800

    Update 03-stage6-link-user-profile-template.sql

--- supabase/sql/03-stage6-link-user-profile-template.sql
+++ supabase/sql/03-stage6-link-user-profile-template.sql
@@ -59,7 +59,7 @@ begin
 
                 (
                     'rudigamer126@gmail.com',
-                    'locdaily',
+                    'owner',
                     'owner'
                 )
 
