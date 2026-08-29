7bfa2a4ce18fc9994bb76a529b1113f5673aadee
Soukyy522 <rudigamer126@gmail.com>
2026-08-30 03:02:19 +0800

    Update license-config.js

--- js/license-config.js
+++ js/license-config.js
@@ -6,14 +6,13 @@ window.LDM_LICENSE_CONFIG = Object.freeze({
     appVersion: "23.1.0",
 
     publicSigningJwk: {
-      "key_ops": [
-            "verify"
-      ],
-      "ext": true,
-      "kty": "EC",
-      "x": "BrphxleiuzWEJq9OU6buA3VbVKYN6TeGl4F2RqD53v4",
-      "y": "p9aJ5rdQwyJ0vYm41jjvAUVYH-NDL_JXhjajl3E2xJs",
-      "crv": "P-256"
-            
-    }
+  "key_ops": [
+    "verify"
+  ],
+  "ext": true,
+  "kty": "EC",
+  "x": "A8QPMQ61kNdvLzz_xeiFhB3X1Qv6a9wzeQkXtioKXj4",
+  "y": "QNbzUZuuDTqEwQfEs3H-e3XU9AfzspK2MQfk1MO4TIk",
+  "crv": "P-256"
+}
 });
