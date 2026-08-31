0fd9da43aa3b563b6e5bff38ec7ddc1a21cf53d6
Soukyy522 <rudigamer126@gmail.com>
2026-08-31 10:44:27 +0800

    Add files via upload

--- js/reporting-service.js
+++ js/reporting-service.js
@@ -1,6 +1,7 @@
 (function(){
     "use strict";
 
+    const SERVICE_VERSION = "25.2.1";
     const ENABLED_KEY = "ldmReportingCloudEnabled";
     const LAST_SYNC_KEY = "ldmReportingLastSyncAt";
     const EXPENSE_BUCKET = "ldm-expense-receipts";
@@ -1430,6 +1431,7 @@
     }
 
     window.LDMReporting = Object.freeze({
+        version:SERVICE_VERSION,
         createUUID,
         stableNumericId,
         isEnabled,
